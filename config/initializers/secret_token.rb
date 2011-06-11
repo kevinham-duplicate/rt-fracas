5dab939cc42e3b3c018ef7e0bc51e6cc43e0642f
Kevin Ham <kaiwen123Wgmail.com>
2011-06-11 08:53:21 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RtFracas::Application.config.secret_token = '94003e77f2afce23aec49f1fa7eb821b28e7910e173d3e566a298e3482e8e28bd8de29d2a05aea7835b75738af2e4ff0d83de2cd2cffff1d703da42886eb208f'
